d0619d54e91d9829f54c50b9a9d8637588859b56
Michael Anthony <mike365@uw.edu>
2017-02-14 22:26:47 -0800

    Update CoffeeShopService.java

--- src/main/java/com/uw/coffeeshop/CoffeeShopService.java
+++ src/main/java/com/uw/coffeeshop/CoffeeShopService.java
@@ -11,4 +11,22 @@ package com.uw.coffeeshop;
  */
 public class CoffeeShopService {
     
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String createShop(String job) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Review shop = mapper.readValue(job.toString(), CoffeeShop.class);
+        
+        StringBuilder text = new StringBuilder();
+        text.append("The JSON obj:" + job.toString() + "\n");
+        text.append("The shop's title is " + shop.getName() + "\n");
+        text.append("The address of this shop is " + shop.getAddress()+ " Name.\n");
+        text.append("Coffee Shop:\n");
+        for (Object msg : shop.getShops())
+            text.append(msg.toString() + "\n");
+        
+        return text.toString();
+    }
+    
 }
